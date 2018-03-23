e3483151be72e8280723a01dce145e4f67ed5850
Michel SZYCHIEWICZ <michszychiewicz@gmail.com>
2018-03-23 18:50:48 +0100

    bla

---
+++ case.py
@@ -0,0 +1,5 @@
+class case:
+    def __init__(self, type, location, neighbour):
+        self.type = type # -1
+        self.location = location # [5,5] soit [x,y]
+        self.neighbour = neighbour # [-1,0,3,...,-1]
