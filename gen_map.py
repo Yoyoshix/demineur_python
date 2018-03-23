7fa0685f5361e0d138767b0dc660b392afde9a70
unknown <tyoshim@gmail.com>
2018-03-23 18:20:34 +0100

    little update

--- gen_map.py
+++ gen_map.py
@@ -13,18 +13,18 @@ def verify_param(value, min, max, name):
         return (0)
     return (1)
 
-def get_neighbour(values, x, y):
+def get_neighbour(cell_map, x, y):
     res = 0
-    for i in range( -(x != 0), 1 + (x != len(values) - 1)):
-        for j in range( -(y != 0), 1 + (y != len(values[0]) - 1)):
+    for i in range( -(x != 0), 1 + (x != len(cell_map) - 1)):
+        for j in range( -(y != 0), 1 + (y != len(cell_map[0]) - 1)):
             #print(res, x, y, x + i, y + j)
-            res += (values[x + i][y + j] == -1)
+            res += (cell_map[x + i][y + j] == -1)
     return (res)
 
 #todo generate density
 def generate_map(hei, wid, bomb):
     nb_cell = hei * wid
-    values = np.zeros((hei, wid))
+    cell_map = np.zeros((hei, wid))
     nb = [i for i in range(nb_cell)]
     #bomb_pos = []
     #table_density = np.zeros((hei, wid))
@@ -37,18 +37,18 @@ def generate_map(hei, wid, bomb):
         x = rdm % wid
         nb[rdm] = nb[nb_cell - i - 1]
         #isok = True #((table_density[y][x] / density) >
-        values[y][x] = -1
+        cell_map[y][x] = -1
 
         #for j in range(hei):
         #    for k in range(wid):
         #        table_density[j][k] = (1 - table_density[j][k]) ** (abs(j - y) + abs(k - x))
 
-    #print(values, '\n')
+    #print(cell_map, '\n')
     for i in range(hei):
         for j in range(wid):
-            if (values[i][j] != -1):
-                values[i][j] = get_neighbour(values, i, j)
+            if (cell_map[i][j] != -1):
+                cell_map[i][j] = get_neighbour(cell_map, i, j)
 
-    return (values)
+    return (cell_map)
 
 print(generate_map(8, 8, int(8*8*0.2)))
